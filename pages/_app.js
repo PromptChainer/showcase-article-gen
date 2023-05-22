0a6599304ab7c2d5ba18faab1bc813bca1061e34
Orit Veiner <orit.veiner@gmail.com>
2023-05-22 17:55:04 +0300

    Modify analytics file location

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import GoogleAnalytics from "../components/GoogleAnalytics"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  {process.env.NEXT_PUBLIC_GA_TRACKING_ID && <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID} />}
+  <Component {...pageProps} />
+  </>
+  );
 }
